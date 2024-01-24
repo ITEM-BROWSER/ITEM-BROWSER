72ce4c148acdca7b447ec67e2863cf4ccb6f8b6e
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-01-24 17:28:28 +0900

    refactor: nonNull 로 좀 더 가독성 있게 변경

--- src/main/java/com/psj/itembrowser/common/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/psj/itembrowser/common/filter/JwtAuthenticationFilter.java
@@ -1,6 +1,12 @@
 package com.psj.itembrowser.common.filter;
 
 import com.psj.itembrowser.common.config.jwt.JwtProvider;
+import java.io.IOException;
+import java.util.Objects;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,26 +17,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Objects;
-
 /**
- * packageName    : com.psj.itembrowser.common.filter
- * fileName       : CustomAuthenticationFilter
- * author         : ipeac
- * date           : 2024-01-14
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2024-01-14        ipeac       최초 생성
+ * packageName    : com.psj.itembrowser.common.filter fileName       : CustomAuthenticationFilter
+ * author         : ipeac date           : 2024-01-14 description    :
+ * =========================================================== DATE              AUTHOR NOTE
+ * ----------------------------------------------------------- 2024-01-14        ipeac       최초 생성
  */
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    
     @Autowired
     private JwtProvider jwtProvider;
     
@@ -38,16 +33,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private UserDetailsService userDetailsService;
     
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
         String authorization = request.getHeader("Authorization");
-        if (!Objects.isNull(authorization)) {
+        if (Objects.nonNull(authorization)) {
             String atk = authorization.substring(7);
             try {
-                String email = jwtProvider.extractUserEmail(atk);
+                String email = jwtProvider.extractUserEmail(atk).trim();
                 UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-                Authentication token = new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+                Authentication token = new UsernamePasswordAuthenticationToken(userDetails, "",
+                    userDetails.getAuthorities());
                 SecurityContextHolder.getContext()
-                        .setAuthentication(token);
+                    .setAuthentication(token);
             } catch (JwtException e) {
                 request.setAttribute("exception", e.getMessage());
             }
