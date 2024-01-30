f2869de7509a3dcdb8702d1f303f5b01ac7c8bfc
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-01-30 22:38:09 +0900

    test: modify the test code to operate normally

--- src/test/java/com/psj/itembrowser/order/service/OrderSelectServiceTest.java
+++ src/test/java/com/psj/itembrowser/order/service/OrderSelectServiceTest.java
@@ -2,7 +2,6 @@ package com.psj.itembrowser.order.service;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
-import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.psj.itembrowser.authorization.service.AuthorizationService;
 import com.psj.itembrowser.member.domain.vo.Member;
 import com.psj.itembrowser.order.domain.dto.OrderResponseDTO;
 import com.psj.itembrowser.order.domain.dto.request.OrderRequestDTO;
@@ -27,45 +27,50 @@ import com.psj.itembrowser.shippingInfos.domain.vo.ShippingInfo;
 
 @ExtendWith(MockitoExtension.class)
 public class OrderSelectServiceTest {
-    
-    @InjectMocks
-    private OrderServiceImpl orderService;
-    
-    @Mock
-    private OrderPersistence orderPersistence;
-    
-    private OrderRequestDTO validOrderRequestDTO;
-    private OrderRequestDTO invalidOrderRequestDTO;
-    
-    private Order validOrder;
-    
-    @BeforeEach
-    public void setUp() {
-        validOrderRequestDTO = OrderRequestDTO.forActiveOrder(1L);
-        invalidOrderRequestDTO = OrderRequestDTO.forActiveOrder(2L);
-        validOrder = Order.createOrder(1L, 1L, OrderStatus.ACCEPT, LocalDateTime.now(), 1L,
-            LocalDateTime.now(), null, null, List.of(mock(OrdersProductRelation.class),
-                mock(OrdersProductRelation.class)),
-            mock(Member.class), mock(ShippingInfo.class));
-    }
-    
-    @Test
-    @DisplayName("주문 정상 조회 후 주문 정보 반환이 올바르게 되는지 테스트")
-    void When_GetOrder_Expect_ThrowNotFoundException() {
-        //given
-        given(orderPersistence.getOrder(validOrderRequestDTO)).willReturn(validOrder);
-        
-        //when
-        OrderResponseDTO result = orderService.getOrder(validOrderRequestDTO);
-        
-        //then
-        assertThat(result.getId()).isEqualTo(validOrder.getId());
-        assertThat(result.getOrdererId()).isEqualTo(validOrder.getOrdererId());
-        assertThat(result.getOrderStatus()).isEqualTo(validOrder.getOrderStatus());
-        assertThat(result.getPaidDate()).isEqualTo(validOrder.getPaidDate());
-        assertThat(result.getShippingInfoId()).isEqualTo(validOrder.getShippingInfoId());
-        assertThat(result.getCreatedDate()).isEqualTo(validOrder.getCreatedDate());
-        assertThat(result.getUpdatedDate()).isEqualTo(validOrder.getUpdatedDate());
-        assertThat(result.getDeletedDate()).isEqualTo(validOrder.getDeletedDate());
-    }
+	
+	@InjectMocks
+	private OrderServiceImpl orderService;
+	
+	@Mock
+	private OrderPersistence orderPersistence;
+	
+	@Mock
+	private AuthorizationService authorizationService;
+	
+	private OrderRequestDTO validOrderRequestDTO;
+	
+	private OrderRequestDTO invalidOrderRequestDTO;
+	
+	private Order validOrder;
+	
+	@BeforeEach
+	public void setUp() {
+		validOrderRequestDTO = OrderRequestDTO.forActiveOrder(1L);
+		invalidOrderRequestDTO = OrderRequestDTO.forActiveOrder(2L);
+		validOrder = Order.createOrder(1L, 1L, OrderStatus.ACCEPT, LocalDateTime.now(), 1L,
+			LocalDateTime.now(), null, null, List.of(mock(OrdersProductRelation.class),
+				mock(OrdersProductRelation.class)),
+			mock(Member.class), mock(ShippingInfo.class));
+	}
+	
+	@Test
+	@DisplayName("주문 정상 조회 후 주문 정보 반환이 올바르게 되는지 테스트")
+	void When_GetOrder_Expect_ThrowNotFoundException() {
+		//given
+		given(orderPersistence.getOrder(validOrderRequestDTO)).willReturn(validOrder);
+		doNothing().when(authorizationService).authorizeOrder(validOrder);
+		
+		//when
+		OrderResponseDTO result = orderService.getOrder(validOrderRequestDTO);
+		
+		//then
+		assertThat(result.getId()).isEqualTo(validOrder.getId());
+		assertThat(result.getOrdererId()).isEqualTo(validOrder.getOrdererId());
+		assertThat(result.getOrderStatus()).isEqualTo(validOrder.getOrderStatus());
+		assertThat(result.getPaidDate()).isEqualTo(validOrder.getPaidDate());
+		assertThat(result.getShippingInfoId()).isEqualTo(validOrder.getShippingInfoId());
+		assertThat(result.getCreatedDate()).isEqualTo(validOrder.getCreatedDate());
+		assertThat(result.getUpdatedDate()).isEqualTo(validOrder.getUpdatedDate());
+		assertThat(result.getDeletedDate()).isEqualTo(validOrder.getDeletedDate());
+	}
 }
