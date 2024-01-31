4c23cd6865fd61ab1bc118e9d11ddc964b84a599
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-01-31 14:56:41 +0900

    refactor: Update for api versioning

--- src/main/java/com/psj/itembrowser/order/controller/OrderApiController.java
+++ src/main/java/com/psj/itembrowser/order/controller/OrderApiController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.psj.itembrowser.common.message.MessageDTO;
@@ -19,19 +20,20 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/v1/api/orders")
 public class OrderApiController {
-	
-	private final OrderService orderService;
-	
-	@GetMapping("/v1/api/orders/{orderId}")
-	public ResponseEntity<OrderResponseDTO> getOrder(@PathVariable Long orderId) {
-		OrderRequestDTO orderRequestDTO = OrderRequestDTO.forActiveOrder(orderId);
-		return ResponseEntity.ok(orderService.getOrder(orderRequestDTO));
-	}
-	
-	@DeleteMapping("/v1/api/orders/{orderId}")
-	public MessageDTO removeOrder(@PathVariable Long orderId) {
-		orderService.removeOrder(orderId);
-		return new MessageDTO(format("Order record for {0} has been deleted.", orderId));
-	}
+    
+    private final OrderService orderService;
+    
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponseDTO> getOrder(@PathVariable Long orderId) {
+        OrderRequestDTO orderRequestDTO = OrderRequestDTO.forActiveOrder(orderId);
+        return ResponseEntity.ok(orderService.getOrder(orderRequestDTO));
+    }
+    
+    @DeleteMapping("/{orderId}")
+    public MessageDTO removeOrder(@PathVariable Long orderId) {
+        orderService.removeOrder(orderId);
+        return new MessageDTO(format("Order record for {0} has been deleted.", orderId));
+    }
 }
