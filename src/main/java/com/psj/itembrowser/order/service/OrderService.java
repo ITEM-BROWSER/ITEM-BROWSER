cd95c96f2e5fb8e2d3c0d2cb737114968c23c082
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-02-04 16:32:11 +0900

    feat: Define service default method

--- src/main/java/com/psj/itembrowser/order/service/OrderService.java
+++ src/main/java/com/psj/itembrowser/order/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.psj.itembrowser.order.service;
 
+import com.github.pagehelper.PageInfo;
+import com.psj.itembrowser.order.domain.dto.request.OrderPageRequestDTO;
 import com.psj.itembrowser.order.domain.dto.response.OrderResponseDTO;
 
 public interface OrderService {
@@ -9,4 +11,8 @@ public interface OrderService {
     OrderResponseDTO getOrderWithNotDeleted(Long id);
     
     OrderResponseDTO getOrderWithNoCondition(Long id);
+    
+    PageInfo<OrderResponseDTO> getOrdersWithPaginationAndNoCondition(OrderPageRequestDTO requestDTO);
+    
+    PageInfo<OrderResponseDTO> getOrdersWithPaginationAndNotDeleted(OrderPageRequestDTO requestDTO);
 }
