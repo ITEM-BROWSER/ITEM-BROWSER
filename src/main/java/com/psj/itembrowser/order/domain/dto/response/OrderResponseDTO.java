383c8a600f816e12f55515220c7e2b4ebe12d82c
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-02-06 00:18:14 +0900

    feat: Add ordersProductRelations relationship to OrderResponseDTO static method

--- src/main/java/com/psj/itembrowser/order/domain/dto/response/OrderResponseDTO.java
+++ src/main/java/com/psj/itembrowser/order/domain/dto/response/OrderResponseDTO.java
@@ -6,8 +6,11 @@ import com.psj.itembrowser.member.domain.dto.response.MemberResponseDTO;
 import com.psj.itembrowser.member.domain.vo.Member;
 import com.psj.itembrowser.order.domain.vo.Order;
 import com.psj.itembrowser.order.domain.vo.OrderStatus;
+import com.psj.itembrowser.order.domain.vo.OrdersProductRelationResponseDTO;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,33 +24,38 @@ import lombok.Setter;
 @NoArgsConstructor(access = PROTECTED)
 @AllArgsConstructor
 public class OrderResponseDTO implements Serializable {
-	
-	private Long id;
-	private Long ordererNumber;
-	private OrderStatus orderStatus;
-	private LocalDateTime paidDate;
-	private Long shippingInfoId;
-	private LocalDateTime createdDate;
-	private LocalDateTime updatedDate;
-	private LocalDateTime deletedDate;
-	
-	private MemberResponseDTO member;
-	
-	public static OrderResponseDTO create(Order order) {
-		OrderResponseDTO orderResponseDTO = new OrderResponseDTO();
-		
-		orderResponseDTO.setId(order.getId());
-		orderResponseDTO.setOrdererNumber(order.getOrdererNumber());
-		orderResponseDTO.setOrderStatus(order.getOrderStatus());
-		orderResponseDTO.setPaidDate(order.getPaidDate());
-		orderResponseDTO.setShippingInfoId(order.getShippingInfoId());
-		orderResponseDTO.setCreatedDate(order.getCreatedDate());
-		orderResponseDTO.setUpdatedDate(order.getUpdatedDate());
-		orderResponseDTO.setDeletedDate(order.getDeletedDate());
-		
-		Member member = order.getMember();
-		orderResponseDTO.setMember(MemberResponseDTO.from(member));
-		
-		return orderResponseDTO;
-	}
+    
+    private Long id;
+    private Long ordererNumber;
+    private OrderStatus orderStatus;
+    private LocalDateTime paidDate;
+    private Long shippingInfoId;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+    private LocalDateTime deletedDate;
+    
+    private MemberResponseDTO member;
+    private List<OrdersProductRelationResponseDTO> ordersProductRelations = new ArrayList<>();
+    
+    public static OrderResponseDTO create(Order order) {
+        OrderResponseDTO orderResponseDTO = new OrderResponseDTO();
+        
+        orderResponseDTO.setId(order.getId());
+        orderResponseDTO.setOrdererNumber(order.getOrdererNumber());
+        orderResponseDTO.setOrderStatus(order.getOrderStatus());
+        orderResponseDTO.setPaidDate(order.getPaidDate());
+        orderResponseDTO.setShippingInfoId(order.getShippingInfoId());
+        orderResponseDTO.setCreatedDate(order.getCreatedDate());
+        orderResponseDTO.setUpdatedDate(order.getUpdatedDate());
+        orderResponseDTO.setDeletedDate(order.getDeletedDate());
+        
+        Member member = order.getMember();
+        orderResponseDTO.setMember(MemberResponseDTO.from(member));
+        
+        order.getProducts().stream()
+            .map(OrdersProductRelationResponseDTO::create)
+            .forEach(orderResponseDTO.getOrdersProductRelations()::add);
+        
+        return orderResponseDTO;
+    }
 }
