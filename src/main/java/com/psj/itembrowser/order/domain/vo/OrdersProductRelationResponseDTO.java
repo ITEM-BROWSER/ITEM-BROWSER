a9730931a223cfa6e80ad35839e58c6895e02f88
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-02-06 00:17:02 +0900

    feat: Create OrderPageRequestDTO

---
+++ src/main/java/com/psj/itembrowser/order/domain/vo/OrdersProductRelationResponseDTO.java
@@ -0,0 +1,46 @@
+package com.psj.itembrowser.order.domain.vo;
+
+import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+/**
+ * DTO for {@link OrdersProductRelation}
+ */
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class OrdersProductRelationResponseDTO {
+    
+    @NotNull
+    Long groupId;
+    
+    @NotNull
+    Long productId;
+    
+    @NotNull
+    @PositiveOrZero
+    Integer productQuantity;
+    
+    LocalDateTime createdDate;
+    LocalDateTime updatedDate;
+    LocalDateTime deletedDate;
+    
+    public static OrdersProductRelationResponseDTO create(
+        @NonNull OrdersProductRelation ordersProductRelation) {
+        
+        return new OrdersProductRelationResponseDTO(
+            ordersProductRelation.getGroupId(),
+            ordersProductRelation.getProductId(),
+            ordersProductRelation.getProductQuantity(),
+            ordersProductRelation.getCreatedDate(),
+            ordersProductRelation.getUpdatedDate(),
+            ordersProductRelation.getDeletedDate()
+        );
+    }
+}
