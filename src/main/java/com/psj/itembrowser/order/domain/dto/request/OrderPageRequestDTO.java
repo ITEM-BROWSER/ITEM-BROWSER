efc8ffc048e7e215efc7ac3a121b30461cc975bf
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-02-06 00:17:34 +0900

    refactor: Add conversion logic for String input value to static method

--- src/main/java/com/psj/itembrowser/order/domain/dto/request/OrderPageRequestDTO.java
+++ src/main/java/com/psj/itembrowser/order/domain/dto/request/OrderPageRequestDTO.java
@@ -2,6 +2,7 @@ package com.psj.itembrowser.order.domain.dto.request;
 
 import com.psj.itembrowser.security.common.pagination.PageRequestDTO;
 import java.time.LocalDate;
+import java.util.Objects;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import lombok.AccessLevel;
@@ -9,12 +10,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Slf4j
 public class OrderPageRequestDTO extends PageRequestDTO {
     
     @NotNull(message = "userNumber must not be null")
@@ -25,13 +28,22 @@ public class OrderPageRequestDTO extends PageRequestDTO {
     private LocalDate requestYear;
     
     public static OrderPageRequestDTO create(PageRequestDTO pageRequestDTO, Long userNumber,
-        LocalDate requestYear) {
+        String requestYearString) {
         OrderPageRequestDTO orderPageRequestDTO = new OrderPageRequestDTO();
         
         orderPageRequestDTO.setPageNum(pageRequestDTO.getPageNum());
         orderPageRequestDTO.setPageSize(pageRequestDTO.getPageSize());
         orderPageRequestDTO.setUserNumber(userNumber);
-        orderPageRequestDTO.setRequestYear(requestYear);
+        
+        if (Objects.nonNull(requestYearString)) {
+            try {
+                int year = Integer.parseInt(requestYearString);
+                LocalDate requestYear = LocalDate.of(year, 1, 1);
+                orderPageRequestDTO.setRequestYear(requestYear);
+            } catch (NumberFormatException e) {
+                log.info("requestYear parse error : {}", e.getMessage());
+            }
+        }
         
         return orderPageRequestDTO;
     }
