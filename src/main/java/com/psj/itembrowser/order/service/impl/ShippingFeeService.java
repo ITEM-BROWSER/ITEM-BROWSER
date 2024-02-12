bd0089535710a4dc0b92bdf10ca9df48cfe17bf3
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-02-12 21:06:33 +0900

    feat: Added error code and added shipping cost related services

---
+++ src/main/java/com/psj/itembrowser/order/service/impl/ShippingFeeService.java
@@ -0,0 +1,19 @@
+package com.psj.itembrowser.order.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *packageName    : com.psj.itembrowser.order.service.impl
+ * fileName       : ShippingFeeService
+ * author         : ipeac
+ * date           : 2024-02-12
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-02-12        ipeac       최초 생성
+ */
+@Service
+public class ShippingFeeService {
+
+}
