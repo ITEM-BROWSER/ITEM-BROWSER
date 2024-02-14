4b466d388fa001a8f46a8db29505fedf1ec74d8b
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-02-14 17:32:10 +0900

    refactor: Changed to basic shipping fee enum

--- src/main/java/com/psj/itembrowser/order/service/impl/ShippingPolicyServiceImpl.java
+++ src/main/java/com/psj/itembrowser/order/service/impl/ShippingPolicyServiceImpl.java
@@ -1,43 +1,33 @@
 package com.psj.itembrowser.order.service.impl;
 
+import com.psj.itembrowser.order.domain.vo.OrdersProductRelationResponseDTO;
+import com.psj.itembrowser.shippingInfos.domain.vo.ShippingPolicy;
+import com.psj.itembrowser.shippingInfos.domain.vo.ShippingPolicy.DeliveryFee;
 import java.util.List;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.psj.itembrowser.order.domain.vo.OrdersProductRelationResponseDTO;
-import com.psj.itembrowser.shippingInfos.domain.vo.ShippingPolicy;
-
-import lombok.extern.slf4j.Slf4j;
-
 /**
- *packageName    : com.psj.itembrowser.order.service.impl
- * fileName       : ShippingFeeService
- * author         : ipeac
- * date           : 2024-02-12
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2024-02-12        ipeac       최초 생성
+ * packageName    : com.psj.itembrowser.order.service.impl fileName       : ShippingFeeService author         : ipeac date           : 2024-02-12 description    : =========================================================== DATE              AUTHOR             NOTE ----------------------------------------------------------- 2024-02-12        ipeac       최초 생성
  */
 @Service
 @Slf4j
 @Transactional(readOnly = true)
 public class ShippingPolicyServiceImpl implements ShippingPolicyService {
-
-	private ShippingPolicy shippingPolicy;
-
-	@Override
-	public ShippingPolicy getCurrentShippingPolicy() {
-		//TODO 매퍼에서 해당 쇼핑 정책에 대한 애그리거트를 호출해서 반환해야합니다  임시 STUB 상태
-		shippingPolicy = new ShippingPolicy(3000, 15000);
-
-		return shippingPolicy;
-	}
-
-	public double calculateShippingFee(List<OrdersProductRelationResponseDTO> totalPrice) {
-
-		return 0;
-	}
+    
+    private ShippingPolicy shippingPolicy;
+    
+    @Override
+    public ShippingPolicy getCurrentShippingPolicy() {
+        //TODO 매퍼에서 해당 쇼핑 정책에 대한 애그리거트를 호출해서 반환해야합니다  임시 STUB 상태
+        shippingPolicy = new ShippingPolicy(DeliveryFee.DEFAULT, 15000);
+        
+        return shippingPolicy;
+    }
+    
+    public double calculateShippingFee(List<OrdersProductRelationResponseDTO> totalPrice) {
+        
+        return 0;
+    }
 }
--- src/main/java/com/psj/itembrowser/shippingInfos/domain/vo/ShippingPolicy.java
+++ src/main/java/com/psj/itembrowser/shippingInfos/domain/vo/ShippingPolicy.java
@@ -1,40 +1,49 @@
 package com.psj.itembrowser.shippingInfos.domain.vo;
 
 import java.io.Serializable;
-
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
- *packageName    : com.psj.itembrowser.shippingInfos.domain.vo
- * fileName       : ShippingPolicy
- * author         : ipeac
- * date           : 2024-02-13
- * description    :
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2024-02-13        ipeac       최초 생성
+ * packageName    : com.psj.itembrowser.shippingInfos.domain.vo fileName       : ShippingPolicy author         : ipeac date           : 2024-02-13 description    : =========================================================== DATE              AUTHOR             NOTE ----------------------------------------------------------- 2024-02-13        ipeac       최초 생성
  */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class ShippingPolicy implements Serializable {
-	/**
-	 * default delivery fee
-	 */
-	private Integer deliveryDefaultFee;
-	/**
-	 * free shipping over amount
-	 */
-	private Integer freeShipOverAmount;
-
-	public double calculateShippingFee(double totalPrice) {
-		if (totalPrice >= freeShipOverAmount) {
-			return 0;
-		}
-		return deliveryDefaultFee;
-	}
+    
+    /**
+     * default delivery fee
+     */
+    private DeliveryFee deliveryDefaultFee;
+    /**
+     * free shipping over amount
+     */
+    private Integer freeShipOverAmount;
+    
+    public DeliveryFee calculateShippingFee(double totalPrice) {
+        Objects.requireNonNull(deliveryDefaultFee, "deliveryDefaultFee must not be null");
+        Objects.requireNonNull(freeShipOverAmount, "freeShipOverAmount must not be null");
+        
+        if (totalPrice >= freeShipOverAmount) {
+            return DeliveryFee.FREE;
+        }
+        return DeliveryFee.DEFAULT;
+    }
+    
+    @Getter
+    public enum DeliveryFee {
+        FREE(0), DEFAULT(3000);
+        
+        private final String name;
+        private final int fee;
+        
+        DeliveryFee(int fee) {
+            this.name = name();
+            this.fee = fee;
+        }
+    }
 }
